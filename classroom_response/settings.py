bc7b530b6317906b17a48c6ad7219b0b8add778f
Kreizhn <kreizh@gmail.com>
2017-09-16 11:08:40 -0400

    Changed admins

--- classroom_response/settings.py
+++ classroom_response/settings.py
@@ -17,6 +17,7 @@ from simpleeval import simple_eval, NameNotDefined
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ADMINS = [('Tyler', 'tyler.holden@utoronto.ca')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
