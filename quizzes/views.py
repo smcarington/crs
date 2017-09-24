2e62d036e0370895435a53d5924b3f46afc443f9
kreizhn <kreizhn@gmail.com>
2017-09-23 22:01:08 -0400

    Another one

--- quizzes/views.py
+++ quizzes/views.py
@@ -1269,7 +1269,7 @@ def change_mark(request):
                 # Update the score. update_score by default adds one to the score,
                 # but takes option argument 'minus' to subtract. We modify based off
                 # the new score.
-                sqr.update_score(res[qnum]['score'])
+                sqr.update_score(int(res[qnum]['score']))
 
                 # Finally, we update the marks
                 update_marks(sqr)
